e8b2fcd54199f5372a2c3e1ed812d537dada67c4
kabolas <suri.mur.bry@gmail.com>
2017-11-17 10:58:18 +0100

    AJout des nouvelles pages pour Dieux et Enchantements

---
+++ RPG_Jahr_words/EnchantControl.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RPG_Jahr_words
+{
+    /// <summary>
+    /// Logique d'interaction pour EnchantControl.xaml
+    /// </summary>
+    public partial class EnchantControl : UserControl
+    {
+        public NameGen Gen
+        {
+            get => (NameGen)GetValue(GenProperty);
+            set => SetValue(GenProperty, value);
+        }
+
+        public static readonly DependencyProperty GenProperty =
+            DependencyProperty.Register("Gen",
+                typeof(NameGen),
+                typeof(PersoControl), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public RPGEntities15 Bdd
+        {
+            get => (RPGEntities15)GetValue(BddProperty);
+            set => SetValue(BddProperty, value);
+        }
+        public static readonly DependencyProperty BddProperty =
+            DependencyProperty.Register("Bdd",
+                typeof(RPGEntities15),
+                typeof(PersoControl), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender, new PropertyChangedCallback(ChargeFromDb)));
+
+        private static void ChargeFromDb(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EnchantControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Generation(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
